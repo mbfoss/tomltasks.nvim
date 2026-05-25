1dc5296939460d839a3b63647d957cb4e0e3997c
mbfoss <mbfoss@fastmail.com>
2026-05-25 21:39:39 +0100

    Update

--- tests/toml-test.sh
+++ tests/toml-test.sh
@@ -1,7 +1,9 @@
 /Users/Dev/homebrew/Cellar/toml-test/2.2.0/bin/toml-test \
 test \
--toml=1.1.0 \
--color=never \
+--toml=1.1.0 \
+--color=never \
 --decoder="nvim -l run_decoder.lua" \
 --encoder="nvim -l run_encoder.lua" \
---skip valid/integer/long --skip valid/integer/float64-max
+--skip valid/integer/long \
+--skip valid/integer/float64-max \
+--skip encoder/integer/long
