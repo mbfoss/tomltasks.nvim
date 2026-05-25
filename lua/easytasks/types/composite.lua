9be8cadf7b5ee1a7190fa8722d1787cb00362027
mbfoss <mbfoss@fastmail.com>
2026-05-26 00:31:08 +0100

    Update composite.lua

--- lua/easytasks/types/composite.lua
+++ lua/easytasks/types/composite.lua
@@ -1,3 +1,5 @@
+local ordered = require("easytasks.util.table_util").ordered
+
 -- Composite tasks have no command of their own; their entire behaviour is
 -- the dependency resolution done by exec.lua before run() is called.
 ---@type easytasks.TaskTypeDef
@@ -9,5 +11,22 @@ return {
     schema = {
         description = "Definition of a `composite` task",
         ["x-order"] = { "name", "type", "save_buffers", "if_running", "depends_on", "depends_order" },
+    },
+
+    templates = {
+        {
+            label = "Task Sequence",
+            task  = ordered(
+                { name = "Sequence", type = "composite", depends_on = { "", "" }, depends_order = "sequence" },
+                { "name", "type", "depends_on", "depends_order" }
+            ),
+        },
+        {
+            label = "Parallel Tasks",
+            task  = ordered(
+                { name = "Parallel", type = "composite", depends_on = { "", "" }, depends_order = "parallel" },
+                { "name", "type", "depends_on", "depends_order" }
+            ),
+        },
     },
 }
